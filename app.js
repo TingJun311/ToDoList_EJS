28d7ab3b46c7d7bdfaa85d7cb68d46cdf8a5dfc7
Isaac <swengineerwithisaac@gmail.com>
2022-06-14 21:38:48 +0800

    Updated app.js

--- app.js
+++ app.js
@@ -1,37 +1,49 @@
-const express = require("express");
-const userData = require("body-parser");
+const express = require("express"); // To user express js
+const userData = require("body-parser"); // To get user data 
 
 const app = express();
-app.set("view engine", "ejs");
+app.set("view engine", "ejs"); // To enable ejs 
 app.listen(3000, () => {
     console.log("Server running on port 3000");
 });
+app.use(userData.urlencoded({
+    // When using the body-parser package
+    extended: true
+}));
+app.use(express.static("public")); // To set the file static to dynamic
 
-let addedItem = [];
-let workListItem = [];
+const addedItem = [], workListItem = [];
+// To store todos item and work list item
+const optionsDate = {
+    // Format for the date
+    weekday: 'short',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+};
+
 app.get("/", (req, res) => {
     const date = new Date();
-    const curDate = getCurDay(date.getDay());
     const curDay = date.toLocaleDateString("en-us", optionsDate);
+    // To format the current date
 
     res.render("list", {
         curDay: curDay,
         newItem: addedItem
     });
 });
-app.use(userData.urlencoded({
-    extended: true
-}))
-app.use(express.static("public"));
-
 app.post("/", (req, res) => {
-    const newItem = req.body.todo;
-
-    console.log(req.body)
+    const newItem = req.body.todo; // Get user data
     if (newItem === null || newItem === "") {
-        res.redirect("/");
+        // Font allow to add if empty
+        res.redirect(
+            (req.body.submitBtn === "Work lists")? "/work" : "/"
+            // Redirect to the current page if empty
+            );
     } else {
         if (req.body.submitBtn === "Work lists") {
+            // Check whether the user add from work or root page
+            // Then redirect them
             workListItem.push(newItem);
             res.redirect("/work");
         } else {
@@ -41,8 +53,8 @@ app.post("/", (req, res) => {
     }  
 });
 
-
 app.get("/work", (req, res) => {
+    // If user enter work list page
     res.render("list", {
         curDay: "Work lists",
         newItem: workListItem
@@ -50,30 +62,3 @@ app.get("/work", (req, res) => {
 });
 
 
-
-
-var optionsDate = {
-    weekday: 'short',
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric'
-};
-
-function getCurDay(day) {
-    switch (day) {
-        case 0:
-            return "Sunday";
-        case 1:
-            return "Monday";
-        case 2:
-            return "Tuesday";    
-        case 3: 
-            return "Wednesday";
-        case 4: 
-            return "Thursday";
-        case 5:
-            return "Friday";
-        default:
-            return "Saturday";    
-    }
-}
